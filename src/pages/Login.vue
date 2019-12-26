ab1981c1ac08b6e462a1447c48713d0b6a910d6e
Hanif Carroll <HanifCarroll@gmail.com>
2019-12-25 23:11:43 -0500

    Add log in form and post request.

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,20 +1,69 @@
 <template>
   <q-page>
     <div class="q-pa-md">
-      <h1>Login</h1>
+      <h4>Login</h4>
+      <q-form
+        @submit="onSubmit"
+        class="q-gutter-md"
+      >
+        <q-input
+          filled
+          type="text"
+          v-model="username"
+          label="Username *"
+        />
+
+        <q-input
+          filled
+          type="password"
+          v-model="password"
+          label="Password *"
+        />
+
+        <div>
+          <q-btn label="Submit" type="submit" color="primary"/>
+        </div>
+      </q-form>
     </div>
   </q-page>
 </template>
 
 <script>
+import * as axios from 'axios';
+
 export default {
   name: 'PageLogin',
 
   data() {
     return {
+      username: null,
+      password: null,
     };
   },
   methods: {
+    onSubmit() {
+      const user = {
+        username: this.username,
+        password: this.password,
+      };
+
+      axios.post('http://localhost:3000/auth/login', user)
+        .then((res) => {
+          console.log('res', res.data);
+
+          this.$router.push({ path: '/table' });
+        })
+        .catch((err) => {
+          console.log('err', err);
+
+          this.$q.notify({
+            color: 'red-4',
+            textColor: 'white',
+            icon: 'error',
+            message: 'Incorrect credentials.',
+          });
+        });
+    },
   },
 };
 </script>
