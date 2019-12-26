7b06003d1a2f0ca7a5f46f7daa55d9077a6e292d
Hanif Carroll <HanifCarroll@gmail.com>
2019-12-26 14:35:40 -0500

    Create table and auth vuex modules.

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-
-// import example from './module-example'
+import auth from './modules/auth';
+import table from './modules/table';
 
 Vue.use(Vuex);
 
@@ -17,7 +17,8 @@ Vue.use(Vuex);
 export default function (/* { ssrContext } */) {
   const Store = new Vuex.Store({
     modules: {
-      // example
+      auth,
+      table,
     },
 
     // enable strict mode (adds overhead!)
