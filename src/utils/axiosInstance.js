d6c5026c985f1632440c5c68607ab652f59d1453
Hanif Carroll <HanifCarroll@gmail.com>
2019-12-27 00:35:59 -0500

    Change baseurl to https

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://134.209.41.168:3000',
+  baseURL: 'https://134.209.41.168:3000',
   // baseURL: 'http://localhost:3000',
   headers: { Authorization: `Bearer ${localStorage.getItem('auth-token')}` },
 });
