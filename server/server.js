bbccd64c8e2ed93c69b842d8a0866cd373d1b2cb
Hanif Carroll <HanifCarroll@gmail.com>
2019-12-27 23:47:58 -0500

    update static directory

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve the static files from the Vue app
-app.use(express.static(path.join(__dirname, '../dist/spa')));
+app.use(express.static(path.join(__dirname, '../dist/spa/statics')));
 
 app.get('*', (req,res) =>{
   res.sendFile(path.join(__dirname, '../dist/spa/index.html'));
