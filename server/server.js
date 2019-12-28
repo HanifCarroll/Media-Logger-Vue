5c8feff6f2f33e700e2f20fc2c29201b12a1bb00
Hanif Carroll <HanifCarroll@gmail.com>
2019-12-27 23:03:05 -0500

    Add server

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve the static files from the Vue app
+app.use(express.static(path.join(__dirname, '../dist/spa')));
+
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname, '../dist/spa/index.html'));
+});
+
+const port = 5000;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
