0ac55bf57dd248c48b8112650a6d6684e4adee01
Hanif Carroll <HanifCarroll@gmail.com>
2020-01-11 17:52:22 -0500

    Replace ids with names.

--- src/store/modules/table.js
+++ src/store/modules/table.js
@@ -43,6 +43,36 @@ export default {
       commit('SET_IS_LOADING', true);
       axiosInstance.get('/media')
         .then((res) => {
+          const tableData = res.data;
+          tableData.forEach((data) => {
+            switch (data.postedBy) {
+              case '398298080874725388':
+                data.postedBy = 'Tu';
+                break;
+              case '141338854312378368':
+                data.postedBy = 'Hanif';
+                break;
+              case '283713846399467522':
+                data.postedBy = 'Jake';
+                break;
+              case '163725284569382914':
+                data.postedBy = 'Trevor';
+                break;
+              case '595029868257869854':
+                data.postedBy = 'James';
+                break;
+              case '482708877276610560':
+                data.postedBy = 'Stefan';
+                break;
+              case '593027116312952834':
+                data.postedBy = 'Alan';
+                break;
+              case '398475220190756875':
+                data.postedBy = 'Adam';
+                break;
+              default:
+            }
+          });
           commit('SET_TABLE_DATA', res.data);
         })
         .catch(() => {
