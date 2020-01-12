5abc5cb98ac299d27ee5eedae7c725c2c423db36
Hanif Carroll <HanifCarroll@gmail.com>
2020-01-11 21:23:05 -0500

    Get auth token with each request

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -3,8 +3,16 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   baseURL: 'https://brahs.media/api',
   // baseURL: 'http://localhost:3000/api',
-  headers: { auth: localStorage.getItem('auth-token') },
 });
+
+axios.interceptors.request.use(
+  (config) => {
+    config.headers.auth = localStorage.getItem('auth-token');
+
+    return config;
+  },
+  error => Promise.reject(error),
+);
 
 axiosInstance.interceptors.response.use(
   response => response,
